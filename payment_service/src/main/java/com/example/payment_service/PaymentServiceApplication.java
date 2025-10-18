b671a12f527ee0afe77288a7ffe80b90a98da3b2
nhducminhqt <nhducminhqt@gmail.com>
2025-10-18 08:26:08 +0700

    feat: update config

--- payment_service/src/main/java/com/example/payment_service/PaymentServiceApplication.java
+++ payment_service/src/main/java/com/example/payment_service/PaymentServiceApplication.java
@@ -3,11 +3,9 @@ package com.example.payment_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableKafka
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
--- payment_service/src/main/java/com/example/payment_service/configuration/PayOSConfiguration.java
+++ payment_service/src/main/java/com/example/payment_service/configuration/PayOSConfiguration.java
@@ -1,9 +1,7 @@
 package com.example.payment_service.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import vn.payos.PayOS;
 
 @Configuration
 public class PayOSConfiguration {
@@ -16,9 +14,4 @@ public class PayOSConfiguration {
 
     @Value("${payos.checksum-key}")
     private String checksumKey;
-
-    @Bean
-    public PayOS payOS() {
-        return new PayOS(clientId, apiKey, checksumKey);
-    }
 }
